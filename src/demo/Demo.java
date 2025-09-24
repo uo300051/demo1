fd2701b30e01482ef44e8501c364489f03b68ff8
uo300051 <uo300051@uniovi.es>
2025-09-24 19:41:08 +0100

    Cambio y borrado
    Cambio en rama
    
    Cambio en rama2 debe aparecer
    
    Merge master en cambio2

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,5 +1,12 @@
 package demo;
 
 public class Demo {
-	private static int b=0;
+	public static void main(String[] args) 
+	{
+		//Cambio en main ahora
+		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
+		/**Cambio en rama222222**/
+	}
 }
