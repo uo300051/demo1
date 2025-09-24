16925e7a6233ea07ff2d591ae444c28a61f67864
uo300051 <uo300051@uniovi.es>
2025-09-24 20:17:12 +0100

    Merge master en cambio2

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,6 +3,7 @@ package demo;
 public class Demo {
 	public static void main(String[] args) 
 	{
+		//Cambio en main ahora
 		System.out.println("linea 1");
 		System.out.println("linea 2");
 		System.out.println("linea 3");
