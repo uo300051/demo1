5dc56af174a96d900db4a9669b0e88d229b69cac
uo300051 <uo300051@uniovi.es>
2025-09-24 19:41:08 +0100

    Cambio y borrado

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,5 +1,10 @@
 package demo;
 
 public class Demo {
-	private static int b=0;
+	public static void main(String[] args) 
+	{
+		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
+	}
 }
